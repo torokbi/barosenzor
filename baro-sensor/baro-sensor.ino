8f2789ec5625bffcf449a60df5fbc8f0ea83bd55
torokbi <bence.torok50@gmail.com>
2023-09-01 15:16:15 +0200

    Review and rewrite the comments

--- baro-sensor/baro-sensor.ino
+++ baro-sensor/baro-sensor.ino
@@ -8,16 +8,20 @@ const char *pin = "1234";
 float datas[10]={};
 
 void setup() {
+
+  // setup serialmonitor and BT
   Serial.begin(115200);
   SerialBT.begin("Baro");
   //SerialBT.setPin(pin);
 
+  // setup BMP180
   if (!bmp.begin()) {
     while (1){
       SerialBT.println("A BMP180 szenzor nem található!");
     };
   }
 
+  // make the avg list
   for (int i=0; i < 10; i++) {
     float pressure = bmp.readPressure() / 100.0F;
     datas[i] = pressure;
@@ -28,25 +32,30 @@ void setup() {
 
 void loop() {
   for (int i=0; i < 10; i++){
-    float pressure = bmp.readPressure() / 100.0F; // olvassuk a légnyomást és számoljuk át hPa-ba
-    float temperature = bmp.readTemperature(); // hőmérséklet kiolvasása
-    //temperature = (temperature - 32.0) * 5.0 / 9.0;  // hőmérséklet átszámítása celziusba
-    
     float sum = 0;
     float avg = 0;
+    
+    // messure
+    float pressure = bmp.readPressure() / 100.0F;
+    float temperature = bmp.readTemperature();
+    
 
+    // calculate the avg of datas
     for (int a=0; a < 10; a++){
       sum = sum + datas[i];
     }
     avg = sum / 10;
   
-  if (pressure > avg*1.4){
-    break;
-  }
+
+    // chek the pressure is correct
+    if (pressure > avg*1.4){
+      break;
+    }
+    // send the beginning
     String str = "POV,P,";
     SerialBT.print("$POV,P,");
     
-    //Nyomásadat elküldése
+    // send pressure
     if (pressure > 0){
       SerialBT.print("+");
       str = str + "+";
@@ -58,7 +67,7 @@ void loop() {
       str = str + String(pressure);
     }
 
-    //hőmérséklet elküldése
+    // send temperature
     SerialBT.print(",T,");
     str = str + ",T,";
 
@@ -73,10 +82,11 @@ void loop() {
       str = str + String(temperature)+"";
     }
 
-    //záró rész elküldése
+    // send checksum
     SerialBT.print("*");
     SerialBT.println(updateChecksum(str));
 
+    // refresh the datas list
     datas[i] = pressure;
 
     delay(200);
